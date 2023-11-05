74337d39bb3a7dafa1e0b69568a931d73ebaa92b
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2023-11-05 10:05:40 +0530

    aditional information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import CanvasModel from "./canvas";
 import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 
-
 function App() {
   return (
     <main className="app transition-all ease-in">
